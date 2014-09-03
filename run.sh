52a423c7191580f7d4e6c20b1ede4caf56a68b2f
huxihao <huxihao@gmail.com>
2014-09-03 15:22:58 +0800

    a

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-##python src/predict_interaction.py WorkPath=work ListFile=500_HBHQ_Docking_Candidates.txt ListFormat=p1/p2/pdb1/ch1/pdb2/ch2/a1/a2 ListSize=5 ThreadNum=2
-python src/predict_interaction.py WorkPath=work NewList=../data/rest_HBHQ_Docking_Candidates.txt ListFormat=p1/p2/pdb1/pdb2/a1/a2 ListSize=5 ThreadNum=12 SkipSize=0 ListSize=5
+python src/predict_interaction.py WorkPath=work NewList=../data/500_HBHQ_Docking_Candidates.txt ListFormat=p1/p2/pdb1/pdb2/a1/a2 ThreadNum=1 ListSize=-1
+python src/predict_interaction.py WorkPath=work NewList=../data/rest_HBHQ_Docking_Candidates.txt ListFormat=p1/p2/pdb1/pdb2/a1/a2 ThreadNum=1 ListSize=-1
