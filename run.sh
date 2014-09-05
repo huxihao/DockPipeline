4ba601dec85aa4b05b8a149df389c8e84a61f1bd
huxihao <huxihao@gmail.com>
2014-09-05 23:25:10 +0800

    a

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mkdir work/zdock_pdbs
 mkdir work/zdock_pool
 cat data/500_HBHQ_Docking_Candidates.txt data/rest_HBHQ_Docking_Candidates.txt > work/full_list.txt
-python src/predict_interaction.py WorkPath=work NewList=full_list.txt ListFormat=p1/p2/pdb1/pdb2/a1/a2 ThreadNum=1 ListSize=1 FeatureType=SaveZDOCK_pdb
+python src/predict_interaction.py WorkPath=work NewList=full_list.txt ListFormat=p1/p2/pdb1/pdb2/a1/a2 ThreadNum=1 ListSize=2
 
