7df22fa3c1bfc3d00deedde182fbfdf10bdbfa5c
huxihao <huxihao@gmail.com>
2014-09-19 17:52:46 +0800

    Update predict_interaction.py
    c

--- src/predict_interaction.py
+++ src/predict_interaction.py
@@ -35,9 +35,9 @@ def main(para):
     ## Step 2: Dock and predict new
     if 'NewList' not in para:
         para['NewList'] = 'list_from_user.txt'
-        para['ListFormat'] = 'a1/a2/p1/p2/pdb1/ch1/pdb2/ch2'
+        para['ListFormat'] = 'p1/p2/a1/a2/pdb1/ch1/pdb2/ch2'
         with open(para['NewList'], 'w') as tempfile:
-            tempfile.write('Hhp1\tTas3\tP49674\tO94687\t4HOK\tA\t3D1D\tA\n')
+            tempfile.write('P49674\tO94687\tHhp1\tTas3\t4HOK\tA\t3D1D\tA\n')
             #tempfile.write('Hhp1\tMoc3\t4HOK\tA\tMOC3_modbase\t \n')
             #tempfile.write('Hhp1\tPpc89\t4HOK\tA\tPPC89_Modbase\t \n')
 
@@ -67,13 +67,12 @@ def main(para):
     outfile = open('PredictedInterfaceResidues_Cutoff%s.txt'%para['PredictCutoff'], 'w')
     cc1 = 0; cc2 = 0
     for line in listfile:
-        ele = line.split()
-        p1 = ele[0]; p2 = ele[1]
-        s1 = ele[2]; s2 = ele[3]
+        p1,p2 = line.split()[:2]
         if (p1,p2) not in pp_val:
             cc2 += 1
             continue
-        outfile.write('\t'.join([p1,p2,s1,s2,'%s--%s--ZDOCK-'%(s1,s2)]))
+        outfile.write(line[:-1]+'\t')
+        outfile.write('%s--%s--ZDOCK-'%(s1,s2))
         cc1 += 1
         res = pp_val[(p1,p2)]
         int1 = [r for r in res if r.split(':')[0]==p1 and res[r] >= float(para['PredictCutoff'])]
